a51531e23ed389560b79db2bc70049de4737ef65
David Haselberger <david.haselberger@univie.ac.at>
2016-05-30 23:12:43 +0200

    * WebView controller started

---
+++ LibraryBox/LBBoxWebViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LBBoxWebViewController.swift
+//  LibraryBox
+//
+//  Created by David on 30/05/16.
+//  Copyright © 2016 Berkman Center. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LBBoxWebViewController: UIViewController
+{
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+}
