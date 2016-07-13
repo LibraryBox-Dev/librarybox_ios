f323dd1ac7cc323bd37a16966cfcc19e725eca45
David Haselberger <david.haselberger@univie.ac.at>
2016-07-13 16:42:28 +0200

    * check if already pinned

--- LibraryBox/LBPinningPopoverViewController.swift
+++ LibraryBox/LBPinningPopoverViewController.swift
@@ -81,60 +81,91 @@ class LBPinningPopoverViewController: UIViewController {
         if let locationForPinning = self.delegate?.currentLocation()
         {
             HUD.show(.Progress)
-            //LBGoogleAPIAccessService.clientId() returns the client ID of the service for the app in the scope
-            let googleConfig = GoogleConfig(
-                clientId: LBGoogleAPIAccessService.clientId(),
-                scopes:["https://www.googleapis.com/auth/fusiontables"])
-            let gdModule =  OAuth2Module(config: googleConfig)
-            self.http.authzModule = gdModule
-            
-            //If access is granted, a new row is set in the Fusion Table associated with LibraryBox locations
-            gdModule.requestAccess { (response:AnyObject?, error:NSError?) -> Void in
+            if(!self.checkForDublicatePinning(locationForPinning))
+            {
+                //LBGoogleAPIAccessService.clientId() returns the client ID of the service for the app in the scope
+                let googleConfig = GoogleConfig(
+                    clientId: LBGoogleAPIAccessService.clientId(),
+                    scopes:["https://www.googleapis.com/auth/fusiontables"])
+                let gdModule =  OAuth2Module(config: googleConfig)
+                self.http.authzModule = gdModule
                 
-                //The acces API key of the app to the service
-                let accessKey:String = LBGoogleAPIAccessService.accessKey()
-                
-                //The content of the Description column in the Fusion Table row
-                let addressTitle:String = ""
-                
-                //The content of the type column in the Fusion Table row
-                let type:String = self.boxTypeSelection.titleForSegmentAtIndex(self.boxTypeSelection.selectedSegmentIndex)!
-                
-                //The SQL query to add the new row in Fusion Table (INSERT INTO table-id (Column, *) VALUES (Value for column, *)
-                let sqlQuery:String = "INSERT INTO 1ICTFk4jdIZIneeHOvhWOcvsZxma_jSqcAWNwuRlK (Description, Latitude, Longitude, Type) VALUES ('\(addressTitle)',\(locationForPinning.coordinate.latitude),\(locationForPinning.coordinate.longitude),'\(type)');"
-                
-                //Transfer sqlQuery string to a URL query string
-                let queryURL: String = sqlQuery.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
-                
-                //The complete URL path string for the Fusion Table query
-                let pathString:String = "https://www.googleapis.com/fusiontables/v2/query?sql=INSERT INTO 1ICTFk4jdIZIneeHOvhWOcvsZxma_jSqcAWNwuRlK (Description, Location, Type) VALUES ('\(addressTitle)', '\(locationForPinning.coordinate.latitude), \(locationForPinning.coordinate.longitude)', '\(type)');&key=\(accessKey)".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
-                
-                //The http request to the REST API of the Fusion Table service
-                self.http.request(.POST, path: pathString, parameters: ["sql":queryURL, "key":accessKey], credential: nil, responseSerializer: StringResponseSerializer(), completionHandler: {(response, error) in
+                //If access is granted, a new row is set in the Fusion Table associated with LibraryBox locations
+                gdModule.requestAccess { (response:AnyObject?, error:NSError?) -> Void in
                     
-                    //Error checking for http request
-                    if (error != nil) {
-                        HUD.hide()
-                        let alert:UIAlertController = UIAlertController(title: "Error", message: "\(error)", preferredStyle: UIAlertControllerStyle.Alert)
-                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-                        self.presentViewController(alert, animated: true, completion: nil)
-                        print("Error uploading file: \(error)")
-                    } else {
-                        HUD.flash(.Success, delay: 1.0)
-                        self.delegate?.locationPinningSuccessful()
-                        delay(2.0)
-                        {
+                    //The acces API key of the app to the service
+                    let accessKey:String = LBGoogleAPIAccessService.accessKey()
+                    
+                    //The content of the Description column in the Fusion Table row
+                    let addressTitle:String = ""
+                    
+                    //The content of the type column in the Fusion Table row
+                    let type:String = self.boxTypeSelection.titleForSegmentAtIndex(self.boxTypeSelection.selectedSegmentIndex)!
+                    
+                    //The SQL query to add the new row in Fusion Table (INSERT INTO table-id (Column, *) VALUES (Value for column, *)
+                    let sqlQuery:String = "INSERT INTO 1ICTFk4jdIZIneeHOvhWOcvsZxma_jSqcAWNwuRlK (Description, Latitude, Longitude, Type) VALUES ('\(addressTitle)',\(locationForPinning.coordinate.latitude),\(locationForPinning.coordinate.longitude),'\(type)');"
+                    
+                    //Transfer sqlQuery string to a URL query string
+                    let queryURL: String = sqlQuery.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+                    
+                    //The complete URL path string for the Fusion Table query
+                    let pathString:String = "https://www.googleapis.com/fusiontables/v2/query?sql=INSERT INTO 1ICTFk4jdIZIneeHOvhWOcvsZxma_jSqcAWNwuRlK (Description, Location, Type) VALUES ('\(addressTitle)', '\(locationForPinning.coordinate.latitude), \(locationForPinning.coordinate.longitude)', '\(type)');&key=\(accessKey)".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+                    
+                    //The http request to the REST API of the Fusion Table service
+                    self.http.request(.POST, path: pathString, parameters: ["sql":queryURL, "key":accessKey], credential: nil, responseSerializer: StringResponseSerializer(), completionHandler: {(response, error) in
+                        
+                        //Error checking for http request
+                        if (error != nil) {
                             HUD.hide()
-                            print("Successfully posted: " + response!.description)
-                            self.dismissViewControllerAnimated(true, completion:{
-                            })
+                            let alert:UIAlertController = UIAlertController(title: "Error", message: "\(error)", preferredStyle: UIAlertControllerStyle.Alert)
+                            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+                            self.presentViewController(alert, animated: true, completion: nil)
+                            print("Error uploading file: \(error)")
+                        } else {
+                            HUD.flash(.Success, delay: 1.0)
+                            self.delegate?.locationPinningSuccessful()
+                            delay(2.0)
+                            {
+                                HUD.hide()
+                                print("Successfully posted: " + response!.description)
+                                self.dismissViewControllerAnimated(true, completion:{
+                                })
+                            }
                         }
-                    }
-                })
+                    })
+                }
+            }
+            else
+            {
+                HUD.hide()
+                let alert:UIAlertController = UIAlertController(title: "Box already pinned", message: "The box is already pinned on the map.", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
             }
         }
-
     }
+    
+    func checkForDublicatePinning(loc: CLLocation) -> Bool
+    {
+        var isDublicate: Bool = true
+        for boxLoc in currentBoxLocations
+        {
+            let locationOfPlace = loc
+            let pinLoc = CLLocation(latitude: boxLoc.coordinate.latitude, longitude: boxLoc.coordinate.longitude)
+            let distance = locationOfPlace.distanceFromLocation(pinLoc)
+            if (distance < 5)
+            {
+                isDublicate = true
+                break
+            }
+            else
+            {
+                isDublicate = false
+            }
+        }
+        return isDublicate
+    }
+
     
     @IBAction func addAddress(sender: AnyObject!)
     {
