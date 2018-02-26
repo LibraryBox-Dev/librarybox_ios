2383351e4a02e8e22f57f4752a65bef6003a38e2
David Haselberger <david.haselberger@gmail.com>
2018-02-26 22:19:39 +0100

    Updating to Swift 4

--- Pods/Target Support Files/Pods-LibraryBoxTests/Pods-LibraryBoxTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LibraryBoxTests : NSObject
-@end
-@implementation PodsDummy_Pods_LibraryBoxTests
-@end
--- Pods/Target Support Files/Pods-LibraryBoxUITests/Pods-LibraryBoxUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LibraryBoxUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LibraryBoxUITests
-@end
